1515ad34cc4969f8f1a96528053a6964beff4329
박유빈 <qkrdbqls1001@naver.com>
2024-04-24 11:24:22 +0900

    feat: 사용자에 대한 조회, 삽입 연산 구현

--- api_server/src/main/java/com/ssafy/double_bean/user/model/repository/UserRepository.java
+++ api_server/src/main/java/com/ssafy/double_bean/user/model/repository/UserRepository.java
@@ -2,45 +2,49 @@ package com.ssafy.double_bean.user.model.repository;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Options;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.ResultMap;
 import org.apache.ibatis.annotations.Results;
 import org.apache.ibatis.annotations.Select;
 
 import com.ssafy.double_bean.user.model.entity.UserEntity;
+import com.ssafy.double_bean.user.model.entity.type_handler.UUIDTypeHandler;
 
 @Mapper
 public interface UserRepository {
-	@Insert("INSERT INTO users(login_id, nickname) VALUES (#{loginId}, #{nickname});")
-	@Options(useGeneratedKeys = true, keyProperty = "id", keyColumn = "id")
-	int insertUser(UserEntity insertDto);
+	@Select("SELECT u.id, uuid, login_id, password, salt, nickname, created_at, u.modified_at "
+			+ "FROM users u LEFT JOIN passwords p ON u.id=p.user_id")
+	@Results(id = "userResult", value = { @Result(property = "id", column = "id"),
+			@Result(property = "uuid", column = "uuid", typeHandler = UUIDTypeHandler.class),
+			@Result(property = "loginId", column = "login_id"), @Result(property = "password", column = "password"),
+			@Result(property = "salt", column = "salt"), @Result(property = "nickname", column = "nickname"),
+			@Result(property = "createdAt", column = "created_at"),
+			@Result(property = "modifiedAt", column = "modified_at"), })
+	List<UserEntity> getAll();
 
-	@Insert("")
-	@Options(useGeneratedKeys = true, keyProperty = "id", keyColumn = "id")
-	int insertPassword(int userId, UserEntity insertDto);
+	@Select("SELECT u.id, uuid, login_id, password, salt, nickname, created_at, u.modified_at "
+			+ "FROM users u LEFT JOIN passwords p ON u.id=p.user_id WHERE login_id=#{loginId}")
+	@ResultMap("userResult")
+	Optional<UserEntity> findByLoginId(String loginId);
+
+	@Select("SELECT u.id, uuid, login_id, password, salt, nickname, created_at, u.modified_at "
+			+ "FROM users u LEFT JOIN passwords p ON u.id=p.user_id WHERE u.id=#{id}")
+	@ResultMap("userResult")
+	Optional<UserEntity> findById(int id);
+
+	@Select("SELECT u.id, uuid, login_id, password, salt, nickname, created_at, u.modified_at "
+			+ "FROM users u LEFT JOIN passwords p ON u.id=p.user_id WHERE uuid='${uuid}'")
+	@ResultMap("userResult")
+	Optional<UserEntity> findByUuid(@Param("uuid") UUID uuid);
 
 	@Insert({ "INSERT INTO users(login_id, nickname) VALUES (#{loginId}, #{nickname}); ",
 			"INSERT INTO passwords(user_id, password, salt) VALUES (LAST_INSERT_ID(), #{password}, #{salt});" })
 	@Options(useGeneratedKeys = true, keyProperty = "id", keyColumn = "id")
 	int insert(UserEntity dto);
-
-	@Select("SELECT u.id, uuid, login_id, password, salt, nickname, created_at, u.modified_at "
-			+ "FROM users u LEFT JOIN passwords p ON u.id=p.user_id WHERE login_id=#{loginId}")
-	Optional<UserEntity> findByLoginId(String loginId);
-
-	@Select("SELECT u.id, uuid, login_id, password, salt, nickname, created_at, u.modified_at "
-			+ "FROM users u LEFT JOIN passwords p ON u.id=p.user_id WHERE u.id=#{id}")
-	Optional<UserEntity> findById(int id);
-
-	@Select("SELECT u.id, uuid, login_id, password, salt, nickname, created_at, u.modified_at "
-			+ "FROM users u LEFT JOIN passwords p ON u.id=p.user_id")
-	@Results(id = "userResult", value = { @Result(property = "id", column = "u.id"),
-			@Result(property = "uuid", column = "uuid"), @Result(property = "loginId", column = "login_id"),
-			@Result(property = "password", column = "password"), @Result(property = "salt", column = "salt"),
-			@Result(property = "nickname", column = "nickname"), @Result(property = "createdAt", column = "created_at"),
-			@Result(property = "modifiedAt", column = "modified_at"), })
-	List<UserEntity> getAll();
 }
