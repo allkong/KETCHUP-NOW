163c55a5cd76438a09ac62bd3b2a93f94bb98408
pcjs156 <qkrdbqls1001@gmail.com>
2024-05-01 19:47:04 +0900

    refactor: Sonarlint 검사 결과 중 수용 가능한 일부 항목 정정

--- api_server/src/main/java/com/ssafy/double_bean/util/DatetimeUtil.java
+++ api_server/src/main/java/com/ssafy/double_bean/util/DatetimeUtil.java
@@ -1,9 +1,11 @@
 package com.ssafy.double_bean.util;
 
 public class DatetimeUtil {
-	public static long SECONDS = 1000L;
-	public static long MINUTES = 1000L * 60;
-	public static long HOURS = 1000L * 60 * 60;
-	public static long DAYS = 1000L * 60 * 60 * 24;
-	public static long MONTHS = 1000L * 60 * 60 * 24 * 30;
+    public static final long SECONDS = 1000L;
+    public static final long MINUTES = 1000L * 60;
+    public static final long HOURS = 1000L * 60 * 60;
+    public static final long DAYS = 1000L * 60 * 60 * 24;
+    public static final long MONTHS = 1000L * 60 * 60 * 24 * 30;
+    private DatetimeUtil() {
+    }
 }
