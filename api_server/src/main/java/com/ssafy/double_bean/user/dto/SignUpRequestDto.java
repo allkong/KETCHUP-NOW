cfe1b3b27c31a60cf291ca33c203ae569fe94ce1
pcjs156 <qkrdbqls1001@gmail.com>
2024-05-01 21:15:47 +0900

    docs(user): 회원 가입 API 요청 DTO 문서 추가

--- api_server/src/main/java/com/ssafy/double_bean/user/dto/SignUpRequestDto.java
+++ api_server/src/main/java/com/ssafy/double_bean/user/dto/SignUpRequestDto.java
@@ -1,24 +1,29 @@
 package com.ssafy.double_bean.user.dto;
 
 import com.ssafy.double_bean.user.model.entity.UserEntity;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
+@Schema(description = "회원 가입 요청 DTO")
 public record SignUpRequestDto(
+        @Schema(description = "로그인 ID", example = "ssafy")
         @NotBlank(message = "loginId cannot be null or blank")
-        @Size(min=1, max = 20, message = "Length of loginId must between [1, 20]")
+        @Size(min = 1, max = 20, message = "Length of loginId must between [1, 20]")
         String loginId,
+        @Schema(description = "비밀번호", example = "password123!")
         @NotBlank(message = "password cannot be null or blank")
-        @Size(min=3, max = 30, message = "Length of password must between [3, 30]")
+        @Size(min = 3, max = 30, message = "Length of password must between [3, 30]")
         String password,
+        @Schema(description = "닉네임", example = "김싸피")
         @NotBlank(message = "nickname cannot be null or blank")
-        @Size(min=3, max = 30, message = "Length of nickname must between [3, 30]")
+        @Size(min = 3, max = 30, message = "Length of nickname must between [3, 30]")
         String nickname) {
-        public UserEntity toRequestEntity() {
-                UserEntity entity = new UserEntity();
-                entity.setLoginId(loginId);
-                entity.setNickname(nickname);
-                entity.setPassword(password);
-                return entity;
-        }
+    public UserEntity toRequestEntity() {
+        UserEntity entity = new UserEntity();
+        entity.setLoginId(loginId);
+        entity.setNickname(nickname);
+        entity.setPassword(password);
+        return entity;
+    }
 }
