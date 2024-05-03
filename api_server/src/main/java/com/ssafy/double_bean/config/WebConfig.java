c2cf8318c012052c6c84f894227da29c4942e368
박유빈 <qkrdbqls1001@naver.com>
2024-05-03 09:07:52 +0900

    feat: 개발 환경 CORS 설정

---
+++ api_server/src/main/java/com/ssafy/double_bean/config/WebConfig.java
@@ -0,0 +1,25 @@
+package com.ssafy.double_bean.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+	@Value("${spring.profiles.active}")
+	private String activeProfile;
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		CorsRegistration defaultRegistry = registry.addMapping("/**")
+				.allowedMethods("OPTIONS", "GET", "POST", "PUT", "DELETE")
+				.allowedHeaders("Authorization", "Content-Type", "Access-Control-Allow-Origin").allowCredentials(true)
+				.exposedHeaders("Authorization").maxAge(3600);
+
+		if (activeProfile.equals("dev")) {
+			defaultRegistry.allowedOrigins("http://localhost:3030/");
+		}
+	}
+}
