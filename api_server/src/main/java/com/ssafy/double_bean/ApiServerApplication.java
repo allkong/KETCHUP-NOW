7f42093d091ebf45792628fd2f3600b672200e07
박유빈 <qkrdbqls1001@naver.com>
2024-05-07 09:44:35 +0900

    chore: Add exception throwing code for testing sentry integration

--- api_server/src/main/java/com/ssafy/double_bean/ApiServerApplication.java
+++ api_server/src/main/java/com/ssafy/double_bean/ApiServerApplication.java
@@ -3,9 +3,18 @@ package com.ssafy.double_bean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.lang.Exception;
+import io.sentry.Sentry;
+
 @SpringBootApplication
 public class ApiServerApplication {
 	public static void main(String[] args) {
+		try {
+			throw new Exception("This is a test.");
+		} catch (Exception e) {
+			Sentry.captureException(e);
+		}
+	
 		SpringApplication.run(ApiServerApplication.class, args);
 	}
 }
