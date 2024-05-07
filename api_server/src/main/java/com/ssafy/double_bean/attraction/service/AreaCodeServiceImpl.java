49f9362ff3d17c0ae39247cd2e0ec3f50648ea22
박유빈 <qkrdbqls1001@naver.com>
2024-05-07 09:41:04 +0900

    build: Add sentry settings
    + Fix property variable reading issue

--- api_server/src/main/java/com/ssafy/double_bean/attraction/service/AreaCodeServiceImpl.java
+++ api_server/src/main/java/com/ssafy/double_bean/attraction/service/AreaCodeServiceImpl.java
@@ -1,6 +1,8 @@
 package com.ssafy.double_bean.attraction.service;
 
-import com.ssafy.double_bean.attraction.util.AreaCode;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.json.GsonJsonParser;
 import org.springframework.boot.json.JsonParser;
@@ -9,33 +11,29 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
 
-import java.util.List;
-import java.util.Map;
+import com.ssafy.double_bean.attraction.util.AreaCode;
 
 @Service
 public class AreaCodeServiceImpl implements AreaCodeService {
-    private static final String SUB_AREA_API_URL = "https://apis.data.go.kr/B551011/KorService1/areaCode1?serviceKey={serviceKey}&areaCode={areaCode}&numOfRows=10000&pageNo=1&MobileOS=ETC&MobileApp=AppTest&_type=json";
-    @Value("${openapi.service-key}")
-    private String openApiServiceKey;
+	private static final String SUB_AREA_API_URL = "https://apis.data.go.kr/B551011/KorService1/areaCode1?serviceKey={serviceKey}&areaCode={areaCode}&numOfRows=10000&pageNo=1&MobileOS=ETC&MobileApp=AppTest&_type=json";
+	@Value("${openapi.service-key}")
+	private static String openApiServiceKey;
 
-    @Override
-    public List<String> getSubAreaNames(int areaCode) {
-        RestClient restClient = RestClient.create();
+	@Override
+	public List<String> getSubAreaNames(int areaCode) {
+		RestClient restClient = RestClient.create();
 
-        ResponseEntity<String> response = restClient
-                .get()
-                .uri(SUB_AREA_API_URL, openApiServiceKey, areaCode)
-                .accept(MediaType.APPLICATION_JSON)
-                .retrieve()
-                .toEntity(String.class);
+		ResponseEntity<String> response = restClient.get().uri(SUB_AREA_API_URL, openApiServiceKey, areaCode)
+				.accept(MediaType.APPLICATION_JSON).retrieve().toEntity(String.class);
 
-        JsonParser parser = new GsonJsonParser();
-        List<Map<String, Object>> items = (List<Map<String, Object>>) ((Map<String, Object>) ((Map<String, Object>) ((Map<String, Object>) parser.parseMap(response.getBody()).get("response")).get("body")).get("items")).get("item");
-        return items.stream().map(obj -> (String) obj.get("name")).toList();
-    }
+		JsonParser parser = new GsonJsonParser();
+		List<Map<String, Object>> items = (List<Map<String, Object>>) ((Map<String, Object>) ((Map<String, Object>) ((Map<String, Object>) parser
+				.parseMap(response.getBody()).get("response")).get("body")).get("items")).get("item");
+		return items.stream().map(obj -> (String) obj.get("name")).toList();
+	}
 
-    @Override
-    public List<String> getSubAreaNames(String areaName) {
-        return getSubAreaNames(AreaCode.getByName(areaName).getCode());
-    }
+	@Override
+	public List<String> getSubAreaNames(String areaName) {
+		return getSubAreaNames(AreaCode.getByName(areaName).getCode());
+	}
 }
