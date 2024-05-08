9ba9669bd163f18ccd9bc33da024ddbb70d00548
dabin <dbj01@naver.com>
2024-05-08 12:11:25 +0900

    style: 사이드바 배치

--- frontend/src/views/CreatorView.vue
+++ frontend/src/views/CreatorView.vue
@@ -3,9 +3,14 @@ import MapComponent from '@/components/MapComponent.vue'
 </script>
 
 <template>
-  <a-layout-content>
-    <MapComponent />
-  </a-layout-content>
+  <a-layout>
+    <a-layout-sider width="200" style="background: #fff"
+      >사이드바</a-layout-sider
+    >
+    <a-layout-content>
+      <MapComponent />
+    </a-layout-content>
+  </a-layout>
 </template>
 
 <style scoped></style>
