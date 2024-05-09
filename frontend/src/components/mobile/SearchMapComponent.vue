4182835a81449087c644ca06848693d74e1486c6
dabin <dbj01@naver.com>
2024-05-09 18:02:45 +0900

    feat(search): mobile map component

---
+++ frontend/src/components/mobile/SearchMapComponent.vue
@@ -0,0 +1,45 @@
+<script setup>
+import { ref, computed, onMounted } from 'vue'
+const { VITE_KAKAO_MAP_KEY } = import.meta.env
+
+const mapContainer = ref(null)
+let mapInstance = null
+
+onMounted(() => {
+  loadKakaoMap(mapContainer.value)
+})
+
+const loadKakaoMap = (container) => {
+  const script = document.createElement('script')
+  script.src = `https://dapi.kakao.com/v2/maps/sdk.js?appkey=${VITE_KAKAO_MAP_KEY}&autoload=false`
+  document.head.appendChild(script)
+
+  script.onload = () => {
+    window.kakao.maps.load(() => {
+      const options = {
+        center: new window.kakao.maps.LatLng(37.5789, 126.977), // 지도 중심 좌표
+        level: 3, // 지도 확대 레벨
+        maxLevel: 5, // 지도 축소 제한 레벨
+      }
+
+      mapInstance = new window.kakao.maps.Map(container, options) // 지도 생성
+    })
+  }
+}
+console.log(window.innerHeight - 94)
+const customHeight = computed(() => {
+  return '--test-map': window.innerHeight - 94
+})
+</script>
+
+<template>
+  <div :style="customHeight" class="map-style">
+    <div ref="mapContainer" style="width: 100%; height: 70vh"></div>
+  </div>
+</template>
+
+<style scoped>
+.map-style {
+  height: var(--test-map);
+}
+</style>
