120e3e5646b7d8556616c0b2f0f58b217a5a5656
pcjs156 <qkrdbqls1001@gmail.com>
2024-05-15 20:58:09 +0900

    chore: DDL을 IntelliJ에서 생성한 것으로 정리

--- api_server/src/main/resources/sql/ddl.sql
+++ api_server/src/main/resources/sql/ddl.sql
@@ -1,104 +1,131 @@
-USE double_bean;
-
-CREATE TABLE users
+create table if not exists double_bean.attractions
 (
-    id          INT PRIMARY KEY AUTO_INCREMENT,
-    uuid        VARCHAR(36) NOT NULL DEFAULT (UUID()),
-    login_id    VARCHAR(32) NOT NULL,
-    nickname    VARCHAR(32) NOT NULL,
-    created_at  TIMESTAMP   NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    modified_at TIMESTAMP   NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    CONSTRAINT unique_users_uuid UNIQUE (uuid),
-    CONSTRAINT unique_users_login_id UNIQUE (login_id),
-    CONSTRAINT unique_users_nickname UNIQUE (nickname)
-);
-
-CREATE TABLE passwords
-(
-    id          INT PRIMARY KEY AUTO_INCREMENT,
-    user_id     INT          NOT NULL,
-    password    VARCHAR(128) NOT NULL,
-    salt        VARCHAR(128) NOT NULL,
-    modified_at TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    CONSTRAINT fk_passwords_users_user_id FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE,
-    CONSTRAINT unique_passwords_user_id UNIQUE (user_id)
-);
-
-CREATE TABLE attractions
-(
-    id              INT PRIMARY KEY NOT NULL,
-    addr1           VARCHAR(300)    NOT NULL DEFAULT '',
-    addr2           VARCHAR(300)    NOT NULL DEFAULT '',
-    area_code       INT             NOT NULL,
-    category_1      CHAR(3),
-    category_2      CHAR(5),
-    category_3      CHAR(9),
-    content_type_id VARCHAR(50)     NOT NULL,
-    first_image     VARCHAR(500)    NOT NULL DEFAULT '',
-    second_image    VARCHAR(500)    NOT NULL DEFAULT '',
-    longitude       DOUBLE          NOT NULL,
-    latitude        DOUBLE          NOT NULL,
-    sigungucode     CHAR(3)         NOT NULL DEFAULT '',
-    tel             VARCHAR(300),
-    title           VARCHAR(300)
-);
-
-CREATE TABLE story_bases
-(
-    id         INT PRIMARY KEY AUTO_INCREMENT,
-    uuid       VARCHAR(36) NOT NULL DEFAULT (UUID()),
-    author_id  INT         NOT NULL,
-    created_at TIMESTAMP   NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    CONSTRAINT fk_story_bases_users_author_id FOREIGN KEY (author_id) REFERENCES users (id) ON DELETE CASCADE
-);
-
-CREATE TABLE stories
-(
-    id                  INT PRIMARY KEY AUTO_INCREMENT,
-    uuid                VARCHAR(36) NOT NULL DEFAULT (UUID()),
-    story_base_id       INT         NOT NULL,
-    version             INT         NOT NULL,
-    status              VARCHAR(10) NOT NULL,
-    title               VARCHAR(30) NOT NULL,
-    description         TEXT,
-    sido                VARCHAR(10),
-    gungu               VARCHAR(10),
-    image_uri           VARCHAR(500),
-    thumbnail_image_uri VARCHAR(500),
-    created_at          TIMESTAMP   NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    modified_at         TIMESTAMP   NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    CONSTRAINT fk_stories_story_bases_story_base_id FOREIGN KEY (story_base_id) REFERENCES story_bases (id) ON DELETE CASCADE
-);
-
-CREATE TABLE story_playings
-(
-    id         INT PRIMARY KEY AUTO_INCREMENT,
-    uuid       VARCHAR(36) NOT NULL DEFAULT (UUID()),
-    story_id   INT         NOT NULL,
-    player_id  INT         NOT NULL,
-    created_at TIMESTAMP   NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    cleared_at TIMESTAMP,
-    CONSTRAINT fk_story_playings_stories_story_id FOREIGN KEY (story_id) REFERENCES stories (id) ON DELETE CASCADE,
-    CONSTRAINT fk_story_playings_users_player_id FOREIGN KEY (player_id) REFERENCES users (id) ON DELETE CASCADE
-);
-
-CREATE TABLE spots
-(
-    id                        INT PRIMARY KEY AUTO_INCREMENT,
-    uuid                      VARCHAR(36) NOT NULL DEFAULT (UUID()),
-    story_id                  INT         NOT NULL,
-    latitude                  DOUBLE      NOT NULL,
-    longitude                 DOUBLE      NOT NULL,
-    order_index               DOUBLE      NOT NULL,
-    title                     VARCHAR(50) NOT NULL,
-    description               TEXT,
-    image_uri                 VARCHAR(500),
-    thumbnail_image_uri       VARCHAR(500),
-    created_at                TIMESTAMP   NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    modified_at               TIMESTAMP   NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    event_type                VARCHAR(10),
-    event_image_uri           VARCHAR(500),
-    event_thumbnail_image_uri VARCHAR(500),
-    json_event_content        TEXT,
-    CONSTRAINT fk_spots_story_story_id FOREIGN KEY (story_id) REFERENCES stories (id) ON DELETE CASCADE
+    id              int                     not null
+        primary key,
+    addr1           varchar(300) default '' not null,
+    addr2           varchar(300) default '' not null,
+    area_code       int                     not null,
+    category_1      char(3)                 null,
+    category_2      char(5)                 null,
+    category_3      char(9)                 null,
+    content_type_id varchar(50)             not null,
+    first_image     varchar(200) default '' not null,
+    second_image    varchar(200) default '' not null,
+    longitude       double                  not null,
+    latitude        double                  not null,
+    sigungucode     char(3)      default '' not null,
+    tel             varchar(300)            null,
+    title           varchar(300)            null
 )
+    charset = utf8mb4;
+
+create table if not exists double_bean.users
+(
+    id          int auto_increment
+        primary key,
+    uuid        varchar(36) default (uuid())          not null,
+    login_id    varchar(32)                           not null,
+    nickname    varchar(32)                           not null,
+    created_at  timestamp   default CURRENT_TIMESTAMP not null,
+    modified_at timestamp   default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP,
+    constraint unique_users_login_id
+        unique (login_id),
+    constraint unique_users_nickname
+        unique (nickname),
+    constraint unique_users_uuid
+        unique (uuid)
+)
+    charset = utf8mb4;
+
+create table if not exists double_bean.passwords
+(
+    id          int auto_increment
+        primary key,
+    user_id     int                                 not null,
+    password    varchar(128)                        not null,
+    salt        varchar(128)                        not null,
+    modified_at timestamp default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP,
+    constraint unique_passwords_user_id
+        unique (user_id),
+    constraint fk_passwords_users_user_id
+        foreign key (user_id) references double_bean.users (id)
+            on delete cascade
+)
+    charset = utf8mb4;
+
+create table if not exists double_bean.story_bases
+(
+    id         int auto_increment
+        primary key,
+    uuid       varchar(36) default (uuid())          not null,
+    author_id  int                                   not null,
+    created_at timestamp   default CURRENT_TIMESTAMP not null,
+    constraint fk_story_bases_users_author_id
+        foreign key (author_id) references double_bean.users (id)
+            on delete cascade
+)
+    charset = utf8mb4;
+
+create table if not exists double_bean.stories
+(
+    id                  int auto_increment
+        primary key,
+    uuid                varchar(36) default (uuid())          not null,
+    story_base_id       int                                   not null,
+    version             int                                   not null,
+    status              varchar(10)                           not null,
+    title               varchar(30)                           not null,
+    description         mediumtext                            null,
+    sido                varchar(10)                           null,
+    gungu               varchar(10)                           null,
+    image_uri           varchar(500)                          null,
+    thumbnail_image_uri varchar(500)                          null,
+    created_at          timestamp   default CURRENT_TIMESTAMP not null,
+    modified_at         timestamp   default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP,
+    constraint fk_stories_story_bases_story_base_id
+        foreign key (story_base_id) references double_bean.story_bases (id)
+            on delete cascade
+)
+    charset = utf8mb4;
+
+create table if not exists double_bean.spots
+(
+    id                        int auto_increment
+        primary key,
+    uuid                      varchar(36) default (uuid())          not null,
+    story_id                  int                                   not null,
+    latitude                  double                                not null,
+    longitude                 double                                not null,
+    order_index               double                                not null,
+    title                     varchar(50)                           not null,
+    description               text                                  null,
+    image_uri                 varchar(500)                          null,
+    thumbnail_image_uri       varchar(500)                          null,
+    created_at                timestamp   default CURRENT_TIMESTAMP not null,
+    modified_at               timestamp   default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP,
+    event_type                varchar(10)                           null,
+    event_image_uri           varchar(500)                          null,
+    event_thumbnail_image_uri varchar(500)                          null,
+    json_event_content        text                                  null,
+    constraint fk_spots_story_story_id
+        foreign key (story_id) references double_bean.stories (id)
+            on delete cascade
+);
+
+create table if not exists double_bean.story_playings
+(
+    id         int auto_increment
+        primary key,
+    uuid       varchar(36) default (uuid())          not null,
+    story_id   int                                   not null,
+    player_id  int                                   not null,
+    created_at timestamp   default CURRENT_TIMESTAMP not null,
+    cleared_at timestamp                             null,
+    constraint fk_story_playings_stories_story_id
+        foreign key (story_id) references double_bean.stories (id)
+            on delete cascade,
+    constraint fk_story_playings_users_player_id
+        foreign key (player_id) references double_bean.users (id)
+            on delete cascade
+);
+
