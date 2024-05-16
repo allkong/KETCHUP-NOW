dde15b4cc6e77dc208e9ef2c71733ade7eea8c38
pcjs156 <qkrdbqls1001@gmail.com>
2024-05-16 12:23:11 +0900

    feat: 로그인 API 연동

--- frontend/src/stores/user-store.js
+++ frontend/src/stores/user-store.js
@@ -10,22 +10,23 @@ export const useUserStore = defineStore('userStore', () => {
    * actions
    */
   const login = async (loginInfo) => {
-    await axios
-      .post('/auth/login', loginInfo, { withCredentials: true })
-      // 로그인 성공시
-      .then(async (response) => {
-        console.log(response)
-        // 모든 요청에 Access token이 포함되도록 함
-        const newAccessToken = response.headers.getAuthorization()
-        axios.defaults.headers.common['Authorization'] = newAccessToken
-        // 사용자 정보 state 갱신
-        userInfo.value = (await axios.get('/users/me')).data
-      })
-      .catch((error) => console.error(error))
+    return (
+      axios
+        .post('/auth/login', loginInfo, { withCredentials: true })
+        // 로그인 성공시
+        .then(async (response) => {
+          console.log(response)
+          // 모든 요청에 Access token이 포함되도록 함
+          const newAccessToken = response.headers.getAuthorization()
+          axios.defaults.headers.common['Authorization'] = newAccessToken
+          // 사용자 정보 state 갱신
+          userInfo.value = (await axios.get('/users/me')).data
+        })
+    )
   }
 
   return {
     userInfo,
-    login
+    login,
   }
 })
