ffff03bdb171e31ccee9394428f9332ec2d52169
Dabin Jeong <allempti@gmail.com>
2024-05-19 05:22:47 +0900

    feat(creator): 키워드 검색, 마커 이미지 변경
    - 관광지 마커 이미지 변경
    - 키워드 검색 결과 마커 이미지 변경
    - 키워드 검색 결과 카드 클릭했을 때 해당 장소로 지도 이동 + 마커 이미지 변경 및 크기 확대

--- frontend/src/views/desktop/CreatorView.vue
+++ frontend/src/views/desktop/CreatorView.vue
@@ -1,6 +1,9 @@
 <script setup>
 import { ref, onMounted, inject } from 'vue'
-import { SearchOutlined, FlagOutlined } from '@ant-design/icons-vue'
+import { SearchOutlined, FlagOutlined, RobotOutlined } from '@ant-design/icons-vue'
+import attractionMarkerIcon from '@/assets/icon/marker/star-marker-blue.png'
+import keywordMarkerIcon from '@/assets/icon/marker/star-marker-orange.png'
+import selectedKeywordMarkerIcon from '@/assets/icon/marker/star-marker-pink.png'
 import defaultImage from '@/assets/default-image.jpg'
 const { VITE_KAKAO_MAP_KEY } = import.meta.env
 
@@ -22,12 +25,15 @@ const mapInfo = {
 
 const mapContainer = ref(null)
 let mapInstance = null
-let places = null
-let placeList = ref([])
-let attractionList = ref([])
+
+const attractionList = ref([])
 let attractionMarkers = []
+const selectAttractionTag = ref(false)
+
+let places = null
+const placeList = ref([])
 let keywordMarkers = []
-const selectTag = ref(false)
+let selectedKeywordMarker = null
 
 onMounted(() => {
   onRightCollapse(true)
@@ -65,7 +71,7 @@ const loadKakaoMap = (container) => {
       // 지도 이동 이벤트
       window.kakao.maps.event.addListener(mapInstance, 'dragend', () => {
         // 관광지 버튼이 활성화인 상태로 지도를 이동하면 이동한 지도 영역의 관광지 마커를 생성
-        if (selectTag.value) {
+        if (selectAttractionTag.value) {
           getAttractions()
         }
       })
@@ -96,9 +102,17 @@ const getAttractions = () => {
           attraction.latitude,
           attraction.longitude,
         )
+
+        const attractionMarkerImage = new window.kakao.maps.MarkerImage(
+          attractionMarkerIcon,
+          new window.kakao.maps.Size(35, 35),
+          { offset: new window.kakao.maps.Point(17, 35) },
+        )
+
         const marker = new window.kakao.maps.Marker({
           position: markerPosition,
           title: attraction.title,
+          image: attractionMarkerImage,
         })
         attractionMarkers.push(marker)
         marker.setMap(mapInstance)
@@ -115,7 +129,7 @@ const removeAllMarkers = (markers) => {
 
 // 관광지 버튼 활성화/비활성화
 const handleChange = () => {
-  if (selectTag.value) {
+  if (selectAttractionTag.value) {
     getAttractions()
   } else {
     removeAllMarkers(attractionMarkers)
@@ -127,27 +141,42 @@ const handleChange = () => {
 const searchByKeyword = () => {
   places.keywordSearch(keyword.value, (data, status, pagination) => {
     if (status === window.kakao.maps.services.Status.OK) {
-      placeList.value = data
       removeAllMarkers(keywordMarkers)
-
       let bounds = new window.kakao.maps.LatLngBounds()
 
-      data.forEach((place) => {
+      const keywordMarkerImage = new window.kakao.maps.MarkerImage(
+        keywordMarkerIcon,
+        new window.kakao.maps.Size(35, 35),
+        { offset: new window.kakao.maps.Point(17, 35) },
+      )
+
+      placeList.value = data.map((place) => {
         const placePosition = new window.kakao.maps.LatLng(place.y, place.x)
         const marker = new window.kakao.maps.Marker({
           position: placePosition,
           title: place.place_name,
+          image: keywordMarkerImage,
         })
 
         // LatLngBounds 객체에 좌표 추가
         bounds.extend(placePosition)
 
         // 마커 표시
-        keywordMarkers.push(marker)
         marker.setMap(mapInstance)
-        // 지도 범위 재설정
-        mapInstance.setBounds(bounds)
+        keywordMarkers.push(marker)
+
+        return {
+          ...place,
+          marker,
+        }
       })
+
+      // 지도 범위 재설정
+      mapInstance.setBounds(bounds)
+
+      if (selectAttractionTag.value) {
+        getAttractions()
+      }
     } else if (status === kakao.maps.services.Status.ZERO_RESULT) {
       alert('검색 결과가 존재하지 않습니다.')
       return
@@ -159,8 +188,36 @@ const searchByKeyword = () => {
 }
 
 const replaceDefaultImage = (e) => {
-  console.log(e.target.src)
   e.target.src = defaultImage
+}
+
+const moveToLocation = (place) => {
+  const newPosition = new window.kakao.maps.LatLng(place.y, place.x)
+  mapInstance.setCenter(newPosition)
+
+  if (selectedKeywordMarker) {
+    // 기존에 선택한 마커 이미지 되돌리기
+    const keywordMarkerImage = new window.kakao.maps.MarkerImage(
+      keywordMarkerIcon,
+      new window.kakao.maps.Size(35, 35),
+      { offset: new window.kakao.maps.Point(17, 35) },
+    )
+    selectedKeywordMarker.setImage(keywordMarkerImage)
+  }
+
+  // 새로 선택한 마커 이미지 변경
+  const selectedKeywordMarkerImage = new window.kakao.maps.MarkerImage(
+    selectedKeywordMarkerIcon,
+    new window.kakao.maps.Size(45, 45),
+    { offset: new window.kakao.maps.Point(23, 35) },
+  )
+  place.marker.setImage(selectedKeywordMarkerImage)
+  place.marker.setZIndex(1)
+  selectedKeywordMarker = place.marker
+
+  if (selectAttractionTag.value) {
+    getAttractions()
+  }
 }
 </script>
 
@@ -173,13 +230,16 @@ const replaceDefaultImage = (e) => {
     </a-row> -->
   </a-layout-header>
   <a-layout>
-    <a-layout-sider width="4rem" class="left-inner-side">
+    <a-layout-sider width="4rem" class="left-inner-sider">
       <a-menu v-model:selectedKeys="selectedKeys" theme="light" mode="inline">
         <a-menu-item key="1" @click="openLeftSider">
           <SearchOutlined />
         </a-menu-item>
         <a-menu-item key="2" @click="openLeftSider">
           <FlagOutlined />
+        </a-menu-item>
+        <a-menu-item key="3">
+          <RobotOutlined />
         </a-menu-item>
       </a-menu>
     </a-layout-sider>
@@ -191,8 +251,10 @@ const replaceDefaultImage = (e) => {
       @collapse="onLeftCollapse"
       collapsible
       class="left-sider-shadow"
+      @click=""
     >
-      <div v-show="selectedKeys[0] === '1'">
+      <div v-show="selectedKeys[0] === '1'" style="height: 100%">
+        <h2>키워드 검색</h2>
         <a-row justify="center">
           <a-col>
             <div>
@@ -211,9 +273,14 @@ const replaceDefaultImage = (e) => {
             </div>
           </a-col>
         </a-row>
-        <a-row>
-          <a-card>
-            <a-card-grid v-for="place in placeList" :key="place.id" class="attraction-item">
+        <a-row style="height: 100%">
+          <a-card class="left-outer-sider-cards">
+            <a-card-grid
+              v-for="place in placeList"
+              :key="place.id"
+              class="attraction-item"
+              @click="moveToLocation(place)"
+            >
               <h3>{{ place.place_name }}</h3>
               <p>{{ place.address_name }}</p>
               <p>{{ place.road_address_name }}</p>
@@ -222,10 +289,10 @@ const replaceDefaultImage = (e) => {
           </a-card>
         </a-row>
       </div>
-      <div v-show="selectedKeys[0] === '2'">
+      <div v-show="selectedKeys[0] === '2'" style="height: 100%">
         <h2>관광지 목록</h2>
-        <p v-show="attractionList.length === 0">관광지 버튼을 클릭해 주세요!</p>
-        <a-card>
+        <a-card class="left-outer-sider-cards">
+          <p v-show="attractionList.length === 0">관광지 버튼을 클릭해 주세요!</p>
           <a-card-grid
             v-for="attraction in attractionList"
             :key="attraction.id"
@@ -248,7 +315,7 @@ const replaceDefaultImage = (e) => {
         <div ref="mapContainer" style="width: 100%; height: 100vh"></div>
         <a-checkable-tag
           class="map-button attraction-button"
-          v-model:checked="selectTag"
+          v-model:checked="selectAttractionTag"
           @change="handleChange"
         >
           관광지
@@ -279,16 +346,12 @@ const replaceDefaultImage = (e) => {
   background: lightgray;
 }
 
-.a-layout-sider {
-  z-index: 10;
-}
-
 .ant-layout-sider {
   background: white;
   height: 100vh;
 }
 
-.left-inner-side {
+.left-inner-sider {
   border-inline-end: 1px solid rgba(5, 5, 5, 0.06);
 }
 
@@ -299,6 +362,11 @@ const replaceDefaultImage = (e) => {
 .left-sider-shadow {
   box-shadow: 5px 0 5px -5px rgba(0, 0, 0, 0.2);
   z-index: 1;
+}
+
+.left-outer-sider-cards {
+  overflow: auto;
+  height: 100%;
 }
 
 .right-sider-shadow {
