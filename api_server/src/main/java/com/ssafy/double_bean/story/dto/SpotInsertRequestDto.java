64a5ec949badc4619a68e61245b46f995a989aec
pcjs156 <qkrdbqls1001@gmail.com>
2024-05-20 15:40:14 +0900

    fix(spot): Spot latitude, longitude 입력에서 유효하지 않은 검증 annotation 제거

--- api_server/src/main/java/com/ssafy/double_bean/story/dto/SpotInsertRequestDto.java
+++ api_server/src/main/java/com/ssafy/double_bean/story/dto/SpotInsertRequestDto.java
@@ -1,22 +1,13 @@
 package com.ssafy.double_bean.story.dto;
 
 import com.ssafy.double_bean.story.model.entity.SpotEntity;
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 
 import java.util.UUID;
 
 public record SpotInsertRequestDto(
         UUID previousSpotUuid,
-        @NotNull(message = "latitude is required.")
-        @Min(value = 33, message = "latitude must bigger than 33.0")
-        @Max(value = 43, message = "latitude must smaller than 43.0")
         double latitude,
-        @NotNull(message = "longitude is required.")
-        @Min(value = 124, message = "longitude must bigger than 33.0")
-        @Max(value = 132, message = "longitude must smaller than 43.0")
         double longitude,
         @NotBlank(message = "title is required.")
         String title,
