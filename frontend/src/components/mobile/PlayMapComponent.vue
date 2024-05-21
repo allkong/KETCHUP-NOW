5c48f9c1954aee915b04d3d491c4d6e94a5e5c33
pcjs156 <qkrdbqls1001@gmail.com>
2024-05-21 10:35:24 +0900

    fix: 플레이 화면 벗어나면 위치 추적 중지

--- frontend/src/components/mobile/PlayMapComponent.vue
+++ frontend/src/components/mobile/PlayMapComponent.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, onMounted } from 'vue'
+import { ref, onMounted, onUnmounted } from 'vue'
 import { message } from 'ant-design-vue'
 const { VITE_KAKAO_MAP_KEY } = import.meta.env
 import FindSpot from '@/components/mobile/modal/FindSpot.vue'
@@ -10,16 +10,11 @@ let mapInstance = null
 let currentPositionMarker = null
 const modalOpen = ref(false)
 
-onMounted(() => {
-  loadKakaoMap(mapContainer.value)
-})
-
 const loadKakaoMap = (container) => {
   const script = document.createElement('script')
   script.src = `https://dapi.kakao.com/v2/maps/sdk.js?appkey=${VITE_KAKAO_MAP_KEY}&autoload=false`
   document.head.appendChild(script)
-
-  script.onload = () => {
+  script.addEventListener('load', () => {
     window.kakao.maps.load(() => {
       const options = {
         center: new window.kakao.maps.LatLng(37.5789, 126.977), // 지도 중심 좌표
@@ -28,12 +23,13 @@ const loadKakaoMap = (container) => {
       }
 
       mapInstance = new window.kakao.maps.Map(container, options) // 지도 생성
-
-      displayCurrentPosition()
     })
-  }
+
+    displayCurrentPosition()
+  })
 }
 
+let positionInterval = null
 const displayCurrentPosition = () => {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition((position) => {
@@ -54,35 +50,41 @@ const displayCurrentPosition = () => {
       }
 
       mapInstance.setCenter(currentPosition)
-      watchCurrentPosition()
+      positionInterval = setInterval(() => {
+        navigator.geolocation.getCurrentPosition(
+          (position) => {
+            let lat = position.coords.latitude
+            let lng = position.coords.longitude
+            let newPosition = new window.kakao.maps.LatLng(lat, lng)
+
+            // console.log('현재 위치:', lat, lng)
+            // messageApi.info(`현재 위치: ${lat}, ${lng}`)
+            currentPositionMarker.setPosition(newPosition)
+            mapInstance.setCenter(newPosition)
+
+            // 이동평균
+            // 마커 부드럽게
+          },
+          (error) => {
+            console.error('위치 업데이트 에러:', error)
+          },
+        )
+      }, 1000)
     })
   } else {
     console.log('geolocation 사용 불가')
   }
 }
 
-const watchCurrentPosition = () => {
-  setInterval(() => {
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        let lat = position.coords.latitude
-        let lng = position.coords.longitude
-        let newPosition = new window.kakao.maps.LatLng(lat, lng)
+onMounted(() => {
+  loadKakaoMap(mapContainer.value)
+})
 
-        // console.log('현재 위치:', lat, lng)
-        // messageApi.info(`현재 위치: ${lat}, ${lng}`)
-        currentPositionMarker.setPosition(newPosition)
-        mapInstance.setCenter(newPosition)
-
-        // 이동평균
-        // 마커 부드럽게
-      },
-      (error) => {
-        console.error('위치 업데이트 에러:', error)
-      },
-    )
-  }, 1000)
-}
+onUnmounted(async () => {
+  if (positionInterval) {
+    clearInterval(positionInterval)
+  }
+})
 
 const closeFindSpot = () => {
   modalOpen.value = false
